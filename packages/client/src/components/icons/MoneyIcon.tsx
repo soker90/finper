0c0090e9cc7cc5355b9df395c8e51f6db5a71f2b
Eduardo Parra <edue17@gmail.com>
2022-06-15 08:44:34 +0200

    fix

--- packages/client/src/components/icons/MoneyIcon.tsx
+++ packages/client/src/components/icons/MoneyIcon.tsx
@@ -1,6 +1,4 @@
-import { SVGProps } from 'react'
-
-const MoneyIcon = (props: SVGProps<SVGSVGElement>) => (
+const MoneyIcon = (props: any) => (
     <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 488.6 488.6"
