1d94518c561b75a21ddcae99c4d4643d2084f3dc
Eduardo Parra <eduparra90@gmail.com>
2022-07-23 02:28:27 +0200

    fix

--- packages/api/src/config/db.ts
+++ packages/api/src/config/db.ts
@@ -11,14 +11,13 @@ interface IConnect {
 }
 
 export default {
-  connect: ({ user, pass, host, databaseName, mongoUri, options = {} }: IConnect): void => {
+  connect: ({ user, pass, host, databaseName, mongoUri, port, options = {} }: IConnect): void => {
     const userPass = user && pass ? `${user}:${pass}@` : ''
 
     const hostProperty = ([] as string[]).concat(host)
     const hosts = hostProperty.reduce((s, h, i) => `${s}${i > 0 ? ',' : ''}${h}`, '')
 
-    let uri = mongoUri || `mongodb://${userPass}${hosts}/${databaseName}`
-    uri += '?retryWrites=true&w=majority'
+    const uri = mongoUri || `mongodb://${userPass}${hosts}:${port}/${databaseName}?retryWrites=true&w=majority`
 
     if (process.env.NODE_ENV !== 'test') {
       models.connect(uri, options)
