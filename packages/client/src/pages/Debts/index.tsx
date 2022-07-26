21b553e2b7e7295b47dcd804ecab8f9a5887d189
Eduardo Parra <edue17@gmail.com>
2022-07-26 19:49:25 +0200

    remove simpletable

--- packages/client/src/pages/Debts/index.tsx
+++ packages/client/src/pages/Debts/index.tsx
@@ -1,13 +1,10 @@
 import { Grid } from '@mui/material'
-import SimpleTable from 'components/SimpleTable'
 
 const DashboardDefault = () => {
   return (<Grid container spacing={3}>
         <Grid item xs={12} md={6}>
-            <SimpleTable/>
         </Grid>
         <Grid item xs={12} md={6}>
-            <SimpleTable/>
         </Grid>
     </Grid>)
 }
