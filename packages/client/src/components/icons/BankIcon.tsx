a07e712c02a2903152a32aaece4a8555c46da76d
Eduardo Parra <eduparra90@gmail.com>
2022-07-27 19:08:45 +0200

    Add imagin icon

--- packages/client/src/components/icons/BankIcon.tsx
+++ packages/client/src/components/icons/BankIcon.tsx
@@ -4,6 +4,7 @@ const openbank = lazy(() => import('./OpenBankIcon'))
 const unicaja = lazy(() => import('./UnicajaIcon'))
 const efectivo = lazy(() => import('./MoneyIcon'))
 const n26 = lazy(() => import('./N26Icon'))
+const imagin = lazy(() => import('./ImaginIcon'))
 
 type BankIconsProps = {
     name: string
@@ -14,7 +15,8 @@ const Icons: Record<string, FC<any>> = {
   openbank,
   unicaja,
   efectivo,
-  n26
+  n26,
+  imagin
 }
 
 const BankIcon: FC<BankIconsProps> = ({ name, ...props }) => {
