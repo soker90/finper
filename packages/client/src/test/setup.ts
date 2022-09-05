6bc349a77b6b415735aa546545608279f3bc3850
Eduardo Parra <edue17@gmail.com>
2022-09-05 20:34:25 +0200

    Update setup.ts

--- packages/client/src/test/setup.ts
+++ packages/client/src/test/setup.ts
@@ -1,6 +1,10 @@
 import { afterAll, afterEach, beforeAll } from 'vitest'
 import { server } from '../mock/server'
 
+global.HTMLElement.prototype.detachEvent = function(type, listener) {
+  this.removeEventListener(type.replace('on', ''), listener)
+}
+
 beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
 afterAll(() => server.close())
 afterEach(() => server.resetHandlers())
