5f9503c859b090f7fb9ce283a6e316892ca21a71
Eduardo Parra <edue17@gmail.com>
2022-09-27 21:11:10 +0200

    Add totals of years

---
+++ packages/client/src/pages/Year/components/YearCard.tsx
@@ -0,0 +1,28 @@
+import { Grid, Stack, Typography } from '@mui/material'
+import { MainCard } from 'components'
+import { format } from 'utils'
+
+interface Props {
+    title: string
+    data?: number
+    color?: string
+}
+
+const YearCard = ({ title, data = 0, color }: Props) => (
+    <MainCard contentSX={{ p: 2.25 }}>
+        <Stack spacing={0.5}>
+            <Typography variant="h6" fontWeight='bold' fontSize='medium'>
+                {title}
+            </Typography>
+            <Grid container alignItems="center">
+                <Grid item>
+                    <Typography variant="h4" sx={{ color: `${color || 'primary'}.main` }}>
+                        {format.euro(data)}
+                    </Typography>
+                </Grid>
+            </Grid>
+        </Stack>
+    </MainCard>
+)
+
+export default YearCard
