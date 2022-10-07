98e895241fc6f6b3e7651d936111d544ff59dbb5
Eduardo Parra <edue17@gmail.com>
2022-10-07 21:39:29 +0200

    remove fix happy dom (#69)

--- packages/client/src/test/setup.ts
+++ packages/client/src/test/setup.ts
@@ -1,12 +1,6 @@
 import { afterAll, afterEach, beforeAll, vi } from 'vitest'
 import { server } from '../mock/server'
 
-// https://github.com/capricorn86/happy-dom/issues/467
-// @ts-ignore
-global.HTMLElement.prototype.detachEvent = function (type, listener) {
-  this.removeEventListener(type.replace('on', ''), listener)
-}
-
 global.structuredClone = vi.fn(val => {
   return JSON.parse(JSON.stringify(val))
 })
