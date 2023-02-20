be064794df40ff31aa17219faa5e281c55adb25d
Eduardo Parra <edue17@gmail.com>
2023-02-20 22:54:58 +0100

    fix

--- packages/client/src/components/icons/BankIcon.tsx
+++ packages/client/src/components/icons/BankIcon.tsx
@@ -24,7 +24,7 @@ const Icons: Record<string, FC<any>> = {
   imagin,
   amazon,
   waylet,
-  gourmet
+  gourmet: () => <img src={gourmet} alt='goutmet' />
 }
 
 const BankIcon: FC<BankIconsProps> = ({ name, ...props }) => {
