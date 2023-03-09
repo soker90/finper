bca086f5af8eb31ab35071744771b8850ceea82b
Eduardo Parra <eduparra90@gmail.com>
2023-03-09 20:11:31 +0100

    fix auth api

--- packages/api/src/auth/jwt-strategy-passport-handler.ts
+++ packages/api/src/auth/jwt-strategy-passport-handler.ts
@@ -15,13 +15,15 @@ passport.use(new JwtStrategy({
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   secretOrKey: config.jwt.secret
 }, function (jwtToken, done) {
-  UserModel.findOne({ username: jwtToken.username }, function (err: Error, user: typeof IUser) {
-    if (err) { return done(err, false) }
-
+  UserModel.findOne({ username: jwtToken.username }).then((user: typeof IUser) => {
     if (user) {
       return done(undefined, user, jwtToken)
     }
-
+    return done(undefined, false)
+  }).catch((err: Error) => {
+    if (err) {
+      return done(err, false)
+    }
     return done(undefined, false)
   })
 }))
