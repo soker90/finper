77b153759792a379c059a6f02a5154668bb2b978
Eduardo Parra <edue17@gmail.com>
2023-07-17 18:29:01 +0200

    update deps (#157)

--- packages/client/src/test/testUtils.tsx
+++ packages/client/src/test/testUtils.tsx
@@ -4,18 +4,18 @@ import { render, RenderOptions } from '@testing-library/react'
 import { AuthProvider } from 'contexts/AuthContext'
 import { BrowserRouter } from 'react-router-dom'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import esLocale from 'dayjs/locale/es'
 import ThemeCustomization from 'themes/index'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { SwrProvider } from 'contexts/index'
 import Auth from 'components/Auth'
+import 'dayjs/locale/es'
 
 const AllTheProviders = ({ children }: { children: ReactNode }) => {
   return (
         <AuthProvider>
             <BrowserRouter basename="/">
                 <ThemeCustomization>
-                    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={esLocale}>
+                    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale='es'>
                         <SwrProvider>
                             <Auth>
 
