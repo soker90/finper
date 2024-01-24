5ce634968d135a171979d6b2714b77828f1485a3
Eduardo Parra <eduparra90@gmail.com>
2024-01-24 21:40:45 +0100

    add modal params

--- packages/client/src/pages/Pensions/components/TransactionModal.tsx
+++ packages/client/src/pages/Pensions/components/TransactionModal.tsx
@@ -1,10 +1,9 @@
 import { useForm } from 'react-hook-form'
 import { useSWRConfig } from 'swr'
 
-import { ModalGrid } from 'components/modals'
-import { PensionTransaction } from 'types'
+import { ModalGrid, DateForm, InputForm } from 'components'
+import { type PensionTransaction } from 'types'
 import { PENSIONS } from 'constants/api-paths'
-import InputForm from '../../Accounts/components/AccountEdit/InputForm'
 
 interface Props {
     show: boolean;
@@ -46,8 +45,24 @@ const TransactionModal = ({ show, onClose, transaction, editTransaction, addTran
 
   return (
         <ModalGrid
-            show={show} onClose={onClose} title='Nuevo Movimiento'>
-            <InputForm id='value' label='Importe' placeholder='0'
+            show={show} onClose={onClose} title='Nuevo Movimiento' action={onSubmit}>
+            <DateForm placeholder={'Introduce una fecha'} id='date' label='Fecha'
+                      error={!!errors.date}
+                      control={control}
+            />
+            <InputForm id='value' label='Importe Empresa' placeholder='0'
+                       error={!!errors.companyAmount} {...register('companyAmount', { required: true, valueAsNumber: true })}
+                       errorText='Introduce un número válido'/>
+            <InputForm id='value' label='Unidades Empresa' placeholder='0'
+                       error={!!errors.companyUnits} {...register('companyUnits', { required: true, valueAsNumber: true })}
+                       errorText='Introduce un número válido'/>
+            <InputForm id='value' label='Importe Empleado' placeholder='0'
+                       error={!!errors.employeeAmount} {...register('employeeAmount', { required: true, valueAsNumber: true })}
+                       errorText='Introduce un número válido'/>
+            <InputForm id='value' label='Unidades Empleado' placeholder='0'
+                       error={!!errors.employeeUnits} {...register('employeeUnits', { required: true, valueAsNumber: true })}
+                       errorText='Introduce un número válido'/>
+            <InputForm id='value' label='Valor Unidad' placeholder='0'
                        error={!!errors.value} {...register('value', { required: true, valueAsNumber: true })}
                        errorText='Introduce un número válido'/>
         </ModalGrid>
