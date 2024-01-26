d3294d98a51e32b47f96a2beb7607d9cdd694e6f
Eduardo Parra <eduparra90@gmail.com>
2024-01-26 22:21:02 +0100

    fix test

--- packages/client/src/mock/handlers/auth/login.ts
+++ packages/client/src/mock/handlers/auth/login.ts
@@ -1,29 +1,27 @@
-import { rest, RestContext, RestRequest } from 'msw'
+import { http, HttpResponse } from 'msw'
 
 export enum LoginUsernames {
     // eslint-disable-next-line no-unused-vars
     success = 'success',
 }
 
-interface LoginRequest extends RestRequest{
-    body: {
-        username: LoginUsernames,
-        password: string
-    }
+interface LoginRequest {
+    username: LoginUsernames,
+    password: string
 }
 
 const LOGIN_RESPONSE: Record<LoginUsernames, { token: string }> = {
   [LoginUsernames.success]: {
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QiLCJpYXQiOjE2NTc0MDg5NDEsImV4cCI6MjA1NzQxMjU0MX0.rkv9hF9TF0f9ebqgzEUJlsetHTFPdvzA9oyps1SF1l4'
+    token:
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QiLCJpYXQiOjE2NTc0MDg5NDEsImV4cCI6MjA1NzQxMjU0MX0.rkv9hF9TF0f9ebqgzEUJlsetHTFPdvzA9oyps1SF1l4'
   }
 }
 
 export const loginHandlers = [
-  rest.post('/auth/login', (req: LoginRequest, res: any, ctx: RestContext) => {
-    const { username } = req.body
+  http.post('/auth/login', async ({ request }) => {
+    const body = await request.json()
+    const { username } = body as LoginRequest
 
-    return res(
-      ctx.status(200),
-      ctx.json(LOGIN_RESPONSE[username]))
+    return HttpResponse.json(LOGIN_RESPONSE[username])
   })
 ]
