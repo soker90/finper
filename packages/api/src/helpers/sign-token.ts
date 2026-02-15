ec8645f93368605f5eb76376b48d1b5b9355e0ea
Eduardo Parra <edue17@gmail.com>
2026-02-15 21:22:48 +0100

    Update api libs

--- packages/api/src/helpers/sign-token.ts
+++ packages/api/src/helpers/sign-token.ts
@@ -1,7 +1,7 @@
-import jwt from 'jsonwebtoken'
+import jwt, { SignOptions } from 'jsonwebtoken'
 
 import config from '../config'
 
 export default (params: Record<string, unknown>): string => (
-  jwt.sign(params, config.jwt.secret, { expiresIn: config.jwt.timeout })
+  jwt.sign(params, config.jwt.secret, { expiresIn: config.jwt.timeout } as SignOptions)
 )
