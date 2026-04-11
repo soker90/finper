c4bcef1b8bd7b544ad509ca203f76d75dd42a768
Eduardo Parra <edue17@gmail.com>
2026-04-11 17:28:40 +0200

    Supply/models (#622)
    * feat(models): add property, supply and supply-reading models for utilities #608
    
    * add agents

---
+++ packages/models/test/models/supply-readings.test.ts
@@ -0,0 +1,34 @@
+import { SupplyReadingModel, SupplyReadingDocument, mongoose } from '../../src'
+import createSupplyReading from '../helpers/create-supply-reading'
+
+const testDatabase = require('../test-db')(mongoose)
+
+const testSupplyReading = (expected: SupplyReadingDocument, received: SupplyReadingDocument) => {
+  expect(expected.supplyId.toString()).toBe(received.supplyId.toString())
+  expect(expected.startDate).toBe(received.startDate)
+  expect(expected.endDate).toBe(received.endDate)
+  expect(expected.user).toBe(received.user)
+}
+
+describe('SupplyReading', () => {
+  beforeAll(() => testDatabase.connect())
+
+  afterAll(() => testDatabase.close())
+
+  describe('when there is a new supply reading', () => {
+    let readingData: SupplyReadingDocument
+
+    beforeAll(() => createSupplyReading().then((reading) => {
+      readingData = reading
+    }))
+
+    afterAll(() => testDatabase.clear())
+
+    test('it should contain all the defined properties', async () => {
+      // @ts-ignore - mongoose typing mismatch
+      const readingDocument: SupplyReadingDocument = await SupplyReadingModel.findOne()
+
+      testSupplyReading(readingDocument, readingData)
+    })
+  })
+})
