90f3bee7881d53eefba1f4629a64263ddc6bdb24
Eduardo Parra <eduparra90@gmail.com>
2026-05-02 16:22:20 +0200

    feat(models): add isSystem flag to Category

--- packages/models/src/models/categories.ts
+++ packages/models/src/models/categories.ts
@@ -6,6 +6,7 @@ export interface ICategory {
   type: TransactionType
   parent?: Types.ObjectId
   user: string
+  isSystem?: boolean
 }
 
 export type CategoryDocument = HydratedDocument<ICategory>
@@ -18,7 +19,8 @@ const categorySchema = new Schema<ICategory>({
     enum: [TRANSACTION.Income, TRANSACTION.Expense, TRANSACTION.NotComputable]
   },
   parent: { type: Schema.Types.ObjectId, ref: 'Category' },
-  user: { type: String, required: true }
+  user: { type: String, required: true },
+  isSystem: { type: Boolean, default: false }
 }, { versionKey: false })
 
 export const CategoryModel = model<ICategory>('Category', categorySchema)
