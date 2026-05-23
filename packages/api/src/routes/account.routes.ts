e99ff93d877cc069f8ae4b10f95a37e45d13083d
Eduardo Parra <edue17@gmail.com>
2026-05-23 13:14:18 +0200

    refactor(accounts): migrate account controller to async/await
    - Convert 5 handlers (create, accounts, edit, account, transfer) from
      Promise chain with .then(tap()) to native async/await
    - Remove unused imports: tap, NextFunction, AccountDocument, extractUser
    - Wrap all route handlers with asyncHandler middleware

--- packages/api/src/routes/account.routes.ts
+++ packages/api/src/routes/account.routes.ts
@@ -4,6 +4,7 @@ import loggerHandler from '../utils/logger'
 import { AccountController } from '../controllers/account.controller'
 import { accountService } from '../services'
 import authMiddleware from '../middlewares/auth.middleware'
+import { asyncHandler } from '../middlewares/async-handler'
 
 export class AccountRoutes {
   router: Router
@@ -22,31 +23,31 @@ export class AccountRoutes {
     this.router.post(
       '/',
       authMiddleware,
-      this.accountController.create.bind(this.accountController)
+      asyncHandler(this.accountController.create.bind(this.accountController))
     )
 
     this.router.get(
       '/',
       authMiddleware,
-      this.accountController.accounts.bind(this.accountController)
+      asyncHandler(this.accountController.accounts.bind(this.accountController))
     )
 
     this.router.patch(
       '/:id',
       authMiddleware,
-      this.accountController.edit.bind(this.accountController)
+      asyncHandler(this.accountController.edit.bind(this.accountController))
     )
 
     this.router.get(
       '/:id',
       authMiddleware,
-      this.accountController.account.bind(this.accountController)
+      asyncHandler(this.accountController.account.bind(this.accountController))
     )
 
     this.router.post(
       '/transfer',
       authMiddleware,
-      this.accountController.transfer.bind(this.accountController)
+      asyncHandler(this.accountController.transfer.bind(this.accountController))
     )
   }
 }
