f720355575a1e10bb8f90848058a834cc7181cf8
Eduardo Parra <edue17@gmail.com>
2026-06-04 21:02:10 +0200

    refactor(api): remove orphaned core http layer - routes/controllers/validators (T2a)

--- packages/api/src/validators/subscription/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './validate-subscription-create-params'
-export * from './validate-subscription-edit-params'
-export * from './validate-subscription-exist'
-export * from './validate-subscription-link-params'
